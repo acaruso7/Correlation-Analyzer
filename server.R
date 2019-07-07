90760fabc2b86bdc8bff28937a27a2fe473f28c0
Alex Caruso <alex.caruso772@gmail.com>
2019-07-07 17:28:43 -0400

    improved code structure

--- server.R
+++ server.R
@@ -2,27 +2,37 @@ library(shiny)
 library(ggplot2)
 
 function(input, output) {
-    output$csv = renderTable({
+    getData = reactive({
         inFile = input$file
         if (is.null(inFile))
             return(NULL)
         df = read.csv(inFile$datapath, header = TRUE)
+        return(df)
+    })
+    output$dataHead = renderTable({
+        return(head(getData()))
+    })
+    
+    output$xvars =
+        renderUI({ # send list of available variable name choices to UI
+            selectInput(inputId='selected_xvar', label="Select X Variable", choices=colnames(getData()), selected=input$selected_xvar)
+        })
+    output$yvars =
+        renderUI({
+            selectInput(inputId='selected_yvar', label="Select Y Variable", choices=colnames(getData()), selected=input$selected_yvar)
+        })
+    
 
-        output$xvars = 
-            renderUI({ # send list of available variable name choices to UI
-                selectInput(inputId='selected_xvar', label="Select X Variable", choices=colnames(df), selected=input$selected_xvar)
-            })
-        output$yvars = 
-            renderUI({
-                selectInput(inputId='selected_yvar', label="Select Y Variable", choices=colnames(df), selected=input$selected_yvar)
-            })
+    output$scatter = renderPlot({
         selected_xvar = input$selected_xvar
         selected_yvar = input$selected_yvar
 
-        output$scatter = renderPlot({
-            plot(unlist(df[selected_xvar], use.names=FALSE), unlist(df[selected_yvar], use.names=FALSE), main="Scatterplot", 
-                 xlab=selected_xvar, ylab=selected_yvar, pch=19, col="blue")
-        })
+        df = getData()
+        if (is.null(df)==FALSE)
+            ggplot(df, aes_string(x=selected_xvar, y=selected_yvar)) +
+                geom_point(shape=1, col="blue") +
+                geom_smooth(method=lm) + ggtitle("Scatterplot") + xlab(selected_xvar) + ylab(selected_yvar) +
+                theme(plot.title = element_text(size=18, hjust=0.5))
     })
 }
 
--- ui.R
+++ ui.R
@@ -1,17 +1,22 @@
 library(shiny)
 
 fluidPage(
+    titlePanel("Correlation Analyzer"),
     sidebarLayout(
         sidebarPanel(
-            fileInput("file", "Choose CSV File",
+            fileInput("file", "Choose Clean CSV File to Analyze (must include headers)",
                       accept = c("text/csv","text/comma-separated-values,text/plain",".csv")
             ),
             uiOutput("xvars"), # dropdowns for selecting variable names
-            uiOutput("yvars")
+            uiOutput("yvars"),
+            checkboxGroupInput(inputId='corrType', label="Correlation Type", 
+                               choices=c('Continuous - Continuous (Pearson)', "Categorical - Categorical (Kramer's V)", 
+                                         'Continuous - Categorical (Correlation Ratio)'), 
+                               selected = NULL, inline = FALSE, width = NULL)
         ),
         mainPanel(
-            tableOutput("csv"),
-            plotOutput("scatter")
+            if (is.null(tableOutput("dataHead")) == FALSE)
+                plotOutput("scatter")
         )
     )
 )

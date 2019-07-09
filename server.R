d4ee37d1a455ae4d5af9a658f590647309c63f72
Alex Caruso <alex.caruso772@gmail.com>
2019-07-09 17:51:48 -0400

    fixed async render error

--- server.R
+++ server.R
@@ -1,60 +1,59 @@
-    library(shiny)
-    library(ggplot2)
-    library(reshape2)
+library(shiny)
+library(ggplot2)
+library(reshape2)
+
+function(input, output) {
+    getData = reactive({
+        inFile = input$file
+        if (is.null(inFile)) {
+            return(NULL) 
+        }
+        df = read.csv(inFile$datapath, header = TRUE)
+        return(df)
+    })
     
-    function(input, output) {
-        getData = reactive({
-            inFile = input$file
-            if (is.null(inFile)) {
-                return(NULL) 
-            }
-            df = read.csv(inFile$datapath, header = TRUE)
-            return(df)
+    output$xvars =
+        renderUI({ # send list of available variable name choices to UI
+            selectInput(inputId='selected_xvar', label="Select X Variable", choices=colnames(getData()), selected=input$selected_xvar)
         })
-        
-        output$xvars =
-            renderUI({ # send list of available variable name choices to UI
-                selectInput(inputId='selected_xvar', label="Select X Variable", choices=colnames(getData()), selected=input$selected_xvar)
-            })
-        output$yvars =
-            renderUI({
-                selectInput(inputId='selected_yvar', label="Select Y Variable", choices=colnames(getData()), selected=input$selected_yvar)
-            })
-        output$continuousVars = 
-            renderUI({
-                selectInput(inputId='selected_cont_vars', label="Select Continuous Variables", 
-                            choices=colnames(getData()), multiple=TRUE, selected=colnames(getData()[1:2]))
-            })
-        
-        output$scatter = renderPlot({
-            selected_xvar = input$selected_xvar
-            selected_yvar = input$selected_yvar
+    output$yvars =
+        renderUI({
+            selectInput(inputId='selected_yvar', label="Select Y Variable", choices=colnames(getData()), selected=input$selected_yvar)
+        })
+    output$continuousVars = 
+        renderUI({
+            selectInput(inputId='selected_cont_vars', label="Select Continuous Variables", 
+                        choices=colnames(getData()), multiple=TRUE, selected=colnames(getData()[1]))
+        })
     
-            df = getData()
-            if (is.null(df)==FALSE) {
-                ggplot(df, aes_string(x=selected_xvar, y=selected_yvar)) +
-                    geom_point(shape=1, col="blue") +
-                    geom_smooth(method=lm) + ggtitle("Scatterplot") + xlab(selected_xvar) + ylab(selected_yvar) +
-                    theme(plot.title = element_text(size=18, hjust=0.5))
-                
-                # plot(unlist(df[selected_xvar]), unlist(df[selected_yvar]), main="Scatterplot",
-                #      xlab=selected_xvar, ylab=selected_yvar, pch=19)
-            }
-        })
-        
-        output$heatmap = renderPlot({
-            selected_vars = input$selected_cont_vars
-            
-            df = getData()
-            if (is.null(df)==FALSE) {
-                features = df[,selected_vars]
-                cormat <- round(cor(as.matrix(sapply(features, as.numeric)), use="pairwise.complete.obs"), 2)
-                melted_cormat <- melt(cormat)
-                ggplot(data = melted_cormat, aes(x=Var1, y=Var2, fill=value)) + 
-                    geom_tile()
-            }
-        })
-    }
+    output$scatter = renderPlot({
+        selected_xvar = input$selected_xvar
+        selected_yvar = input$selected_yvar
+
+        df = getData()
+        if (is.null(df)==FALSE) {
+            req(selected_xvar, selected_yvar)
+            ggplot(df, aes_string(x=selected_xvar, y=selected_yvar)) +
+                geom_point(shape=1, col="blue") +
+                geom_smooth(method=lm) + ggtitle("Scatterplot") + xlab(selected_xvar) + ylab(selected_yvar) +
+                theme(plot.title = element_text(size=18, hjust=0.5))
+        }
+    })
+    
+    output$heatmap = renderPlot({
+        selected_vars = input$selected_cont_vars
+
+        df = getData()
+        if (is.null(df)==FALSE) {
+            req(selected_vars)
+            features = df[,selected_vars]
+            cormat <- round(cor(as.matrix(sapply(features, as.numeric)), use="pairwise.complete.obs"), 2)
+            melted_cormat <- melt(cormat)
+            ggplot(data = melted_cormat, aes(x=Var1, y=Var2, fill=value)) +
+                ggtitle("Heatmap") + geom_tile()
+        }
+    })
+}
 
 
 
